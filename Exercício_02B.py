3f5d9e836fe4abee92a1a5c72bbbd813025c23b9
herbertarq <150201052+herbertarq@users.noreply.github.com>
2024-01-25 22:05:50 -0300

    Update Exercício_02B.py

--- Exercício_02B.py
+++ Exercício_02B.py
@@ -1,49 +1,37 @@
-
+# %%
 import pandas as pd
+
+# %%
 import numpy as np 
 import plotly.express as px 
 import streamlit as st
 import datetime
 
+# %%
 #### CARREGAR OS DADOS ####
 Hapvida=pd.read_csv('RECLAMEAQUI_HAPVIDA.csv',sep=',')
 Ibyte=pd.read_csv('RECLAMEAQUI_IBYTE.csv',sep=',')
 Nagem=pd.read_csv('RECLAMEAQUI_NAGEM.csv',sep=',')
 
-Ibyte.head
-
-##VERICANDO STATUS DO DATAFRAME###
-Ibyte.STATUS.value_counts()
-
-##VERICANDO STATUS DO DATAFRAME###
-Nagem.STATUS.value_counts()
-
-##VERICANDO NÚMERO TOTAL DO STATUS###
-df_Hapvida.STATUS.value_counts()
-
+# %%
 ## DATETIME ####
 Hapvida['TEMPO']=pd.to_datetime(Hapvida['TEMPO'])
 Ibyte['TEMPO']=pd.to_datetime(Ibyte['TEMPO'])
 Nagem['TEMPO']=pd.to_datetime(Nagem['TEMPO'])
 
+# %%
 ####CRIAR COLUNA ESTADO EM TODOS OS DATASETS ###
-Hapvida['LOCAL'].iloc[0].split(' - ')[1].strip()
-Ibyte['LOCAL'].iloc[0].split(' - ')[1].strip()
-Nagem['LOCAL'].iloc[0].split(' - ')[1].strip()
-Hapvida
-
-estado_lista=[]
-for i in range(len(Hapvida)):
-    estado_lista.append(Hapvida['LOCAL'].iloc[i].split(' - ',2)[1].strip())
+Hapvida['UF'] = Hapvida['LOCAL'].str[-2:]
+Ibyte['UF'] = Ibyte['LOCAL'].str[-2:]
+Nagem['UF'] = Nagem['LOCAL'].str[-2:]
 
 
-estado_lista
 
 # %%
 Hapvida.head
 
 # %%
-lista_locais=Hapvida['ESTADO'].unique()
+lista_locais=Hapvida['UF'].unique()
 
 
 # %%
@@ -55,16 +43,39 @@ GERAL = pd.concat ( [Hapvida, Ibyte, Nagem])
 GERAL
 
 # %%
+####CRIAR COLUNA EMPRESA EM TODOS OS DATASETS ###
+GERAL ['EMPRESA'] = GERAL['URL'].str.replace(pat=r'https://www.reclameaqui.com.br//',repl='', regex= True)
+GERAL[['URL', 'EMPRESA']]
+GERAL
+
+# %%
+GERAL
+
+# %%
+GERAL.drop('TEMPO',axis=1).sum()
+
+
+# %%
+##VERICANDO NÚMERO TOTAL DO STATUS###
+GERAL.STATUS.value_counts()
+
+# %%
+GERAL.info
+
+# %%
 #### STREAMLIT###
-st.title('DASHBOARD - RECLAME AQUI - IBYTE, HAPVIDA E NAGEM')
+st.set_page_config(page_title="HERBERT DE SOUSA FERREIRA")
+with st.container():
+    st.subheader("DASHBOARD - RECLAME AQU")
+    st.title('HAPVIDA - IBYTE - NAGEM')
+    st.write("Informações sobre reclamações dos clientes por empresa")
 
 # %%
 st.write('Esse dashboard apresenta dados do Reclame Aqui da Ibyte, HapVida e Nagem')
 
 # %%
-lista_locais=GERAL['ESTADO'].unique()
+lista_locais=GERAL['UF'].unique()
 local = st.selectbox(
     'Selecione o local',
     lista_locais)
-
 
